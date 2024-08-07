f01bf41bb35d4ee0d85693a1e852c2c1edaf23d9
aryaa-9 <125448847+aryaa-9@users.noreply.github.com>
2024-08-07 15:26:29 +0900

    Update stabilityanalysis.py

--- stabilityanalysis.py
+++ stabilityanalysis.py
@@ -1,47 +1,43 @@
 import random
-#%matplotlib qt
 import sympy as sp
 import numpy as np
 from matplotlib import cm
 import matplotlib.pyplot as plt
 
 def energyfunc(S):
-    x, y, c, k = S
+    x, y, c, k, a, m, n = S
     
     #Function 1 (Energy function):
-    ans = (1/2)*k*x**2+(1/2)*y**2
+    #ans = (1/2)*k*x**2+(1/2)*y**2
     
     #Function 2: 
     #ans = x**2+(3*x*y)+(3*y**2)
     
     #Function 3:
-    #m=random.randrange(0,10,1)
-    #n=random.randrange(0,10,1)
-    #a=random.randrange(0,10,1)
-    #ans = x**(2*m)+a*y**(2*n)
+    ans = x**(2*m)+a*y**(2*n)
     
     return ans
     
 def Sdot(t, S):
-    x, y, c, k = S
+    x, y, c, k, a, m, n = S
     
     #Function 1 (Damped Harmonic oscillator): 
-    return [y, -k*x-c*(y**3)*(1+(x**2))]
+    #return [y, -k*x-c*(y**3)*(1+(x**2))]
     
     #Function 2: 
     #return [-2*x-3*y+x**2, x+y]
     
     #Function 3: 
-    #return [-x+2*y**3-2*y**4, -x-y+x*y]
+    return [-x+2*y**3-2*y**4, -x-y+x*y]
 
 def ddtenergy(S):
-    x, y, c, k, t= S
+    x, y, c, k, a, m, n, t = S
     
-    dfdx = sp.diff(energyfunc((x, y, c, k)), x)
-    dfdy = sp.diff(energyfunc((x, y, c, k)), y)
+    dfdx = sp.diff(energyfunc((x, y, c, k, a, m, n)), x)
+    dfdy = sp.diff(energyfunc((x, y, c, k, a, m, n)), y)
     
-    xdot=Sdot(t, (x, y, c, k))[0]
-    ydot=Sdot(t, (x, y, c, k))[1]
+    xdot=Sdot(t, (x, y, c, k, a, m, n))[0]
+    ydot=Sdot(t, (x, y, c, k, a, m, n))[1]
     
     
     dfdt = dfdx * xdot + dfdy * ydot
@@ -50,7 +46,7 @@ def ddtenergy(S):
     
 
 def equipts(dSdt, S):
-    x, y, c, k = S
+    x, y, c, k, a, m, n = S
     dxdt, dydt=dSdt
     eq1=sp.Eq(dxdt, 0)
     eq2=sp.Eq(dydt, 0)
@@ -61,7 +57,7 @@ def equipts(dSdt, S):
 
 def evalV(V, S, sols, l):
     results = []
-    x, y, c, k = S
+    x, y, c, k, a, m, n = S
     for i in range(l):
         V00=V.subs({x: sols[i][0], y: sols[i][1]})
         results.append(V00)
@@ -114,16 +110,16 @@ def plotting(X, Y, Z, zlab, title1, title2, sol, i):
    
     
 def main():
-    x, y, k, c, t = sp.symbols('x y k c t')
+    x, y, k, c, t, a, m, n = sp.symbols('x y k c t a m n')
     
     #V(x,y)
-    V = energyfunc((x, y, c, k))
+    V = energyfunc((x, y, c, k, a, m, n))
     #sp.pretty_print(V)
     
-    dSdt = Sdot(t, (x, y, c, k))
+    dSdt = Sdot(t, (x, y, c, k, a, m, n))
     #sp.pretty_print(dSdt)
 
-    sol1 = equipts(dSdt, (x, y, c, k))
+    sol1 = equipts(dSdt, (x, y, c, k, a, m, n))
     l = len(sol1)
     
     
@@ -140,21 +136,26 @@ def main():
     sol=list(set(sol))
     l = len(sol)
     
-    print("\nThe Equilibrium point(s):")
-    for i in range(l):
-        print(f'V{sol[i]} = ', evalV(V, (x, y, c, k), sol, l)[i])
+    results=evalV(V, (x, y, c, k, a, m, n), sol, l)
+    funcs=[]
+    for i in results:
+        f = sp.lambdify((x, y, c, k, a, m, n), i, 'numpy')
+        funcs.append(f)
     
     x.evalf()
     y.evalf()
-    dVdt = ddtenergy((x, y, c, k, t))
-    f = sp.lambdify((x, y, k, c), dVdt, 'numpy')
+    dVdt = ddtenergy((x, y, c, k, a, m, n, t))
+    f = sp.lambdify((x, y, c, k, a, m, n), dVdt, 'numpy')
     
     c = random.randrange(0, 10, 1)
     k = random.randrange(0, 10, 1)
+    a=random.randrange(0,10,1)
+    m=random.randrange(0,10,1)
+    n=random.randrange(0,10,1)
     x = np.linspace(-10, 10, 400)
     y = np.linspace(-10, 10, 400)
     X, Y = np.meshgrid(x, y)
-    Vxy = energyfunc((X, Y, c, k))
+    Vxy = energyfunc((X, Y, c, k, a, m, n))
     plotting(X, Y, Vxy, 'V(x, y)', 'Lyapunov Function, V(x, y)', 'Contour plot of V(x, y)', sol, -1)
     
     for i in range(l):
@@ -167,6 +168,12 @@ def main():
         x = np.linspace(xminval, xmaxval, 400)
         y = np.linspace(yminval, ymaxval, 400)
         X, Y = np.meshgrid(x, y)
-        Z=f(X, Y, c, k)
+        Z=f(X, Y, c, k, a, m, n)
         plotting(X, Y, Z, r'$\frac{d}{dt} V(x,y)$' , 'Surface plot of '+r'$\frac{d}{dt} V(x,y)$', 'Contour plot of'+r'$\frac{d}{dt} V(x,y)$', sol, i)
+      
+        
+    print("\nThe Equilibrium point(s):")
+    for i in range(l):
+        ans=funcs[i](X, Y, c, k, a, m, n)
+        print(f'V{sol[i]} = ', ans)
 main()
